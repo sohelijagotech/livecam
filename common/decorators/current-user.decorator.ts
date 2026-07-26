8c2e1ad7a7fc85557bba7d57d855d18784a919a5
sohelijagotech <soheli.jagotech@gmail.com>
2026-07-26 22:00:52 +0600

    Delete common directory

--- common/decorators/current-user.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user; // { userId, phone, role } set by JwtStrategy.validate
-  },
-);
